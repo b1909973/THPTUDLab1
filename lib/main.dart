954116387a6d7330fe481b454fc871a68602ed44
Nguyen Van Phuong <phuongb909973@student.ctu.edu.vn>
2022-09-25 21:52:11 +0700

    Xay dung trang cac dat hang

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
+import 'ui/orders/orders_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -25,8 +26,8 @@ class MyApp extends StatelessWidget {
               secondary: Colors.deepOrange
             ),
       ),
-   home: const SafeArea(
-        child: CartScreen(),
+  home: const SafeArea(
+child: OrdersScreen(),
 ),
     );
   }
